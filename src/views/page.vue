98d4425aa15d94cfc156ed50581eda8c8f512a7b
付成 <72155369@vivo.com>
2021-10-15 15:51:01 +0800

    init

---
+++ src/views/page.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    demo-page
+    <p v-if="show">现在你看到我了</p>
+
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      show: true,
+    };
+  },
+};
+</script>
+<style lang="scss">
+
+</style>
